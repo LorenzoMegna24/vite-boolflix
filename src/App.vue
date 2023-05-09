e70e564fefa7641985ad72ef9c5572f588cf21a6
LorenzoMegna24 <megnalorenzo0@gmial.com>
2023-05-09 16:35:07 +0200

    Header

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <script >
   import axios from 'axios';
   import { store } from './store'
+  import HeaderComp from './components/HeaderComp.vue';
+
+  export default{
+    name: 'App',
+    components: {
+      HeaderComp,
+    }
+  }
 </script>
 
 <template>
-  
+  <HeaderComp/>
 </template>
 
 <style lang="scss">
-
+@use './style/main.scss';
 </style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,19 @@
+<script >
+  export default{
+    name: 'HeaderComp'
+  }
+</script>
+
+<template>
+  <div class="bg-black p-3 d-flex align-items-center justify-content-between">
+    <h1 class="text-danger me-5">BOOLFLIX</h1>
+    <div class="input-group w-50">
+      <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+      <button class="input-group-text" id="inputGroup-sizing-default">Search</button>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
